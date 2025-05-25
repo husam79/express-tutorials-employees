13563d63a1a6c40967f7e2938b49cdc6ed93cc1d
Husam Burhan <husam.burhan@gmail.com>
2025-05-25 21:16:57 +0300

    adding middleware

--- src/employees/employees.routes.js
+++ src/employees/employees.routes.js
@@ -1,12 +1,25 @@
 import express from 'express';
 import { addEmployee, deleteEmployee, getAllEmployees, getEmployee, patchEmployee, updateEmployee } from './employees.controller.js';
+import { validateWholeEmployee } from '../validators.js';
 
 const router = express.Router();
 
+const extractWholeEmployeeData = (request, response, next) => {
+    try{
+        const employee = request.body;
+        //validate data sent from the user.
+        validateWholeEmployee(employee);
+        request.employee = employee;
+        next();
+    } catch(error){
+        response.status(400).json({success: false, error: error.message});
+    }
+};
+
 router.get('/employees', getAllEmployees);
 router.get('/employees/:id', getEmployee);
-router.post('/employees', addEmployee);
-router.put('/employees/:id', updateEmployee);
+router.post('/employees', extractWholeEmployeeData, addEmployee);
+router.put('/employees/:id', extractWholeEmployeeData, updateEmployee);
 router.delete('/employees/:id', deleteEmployee);
 router.patch('/employees/:id', patchEmployee);
 
