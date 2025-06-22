8d63018af4441a59f1756daeda19ce6700d96844
Husam Burhan <husam.burhan@gmail.com>
2025-06-22 22:21:15 +0300

    fix a typo

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { login, logout, refereshJWTToken } from './auth.controller.js';
 import wrapper from '../shared/wrapper.js';
+import validateToken from '../middlewares/validate-token.js';
 
 const router = express.Router();
 
